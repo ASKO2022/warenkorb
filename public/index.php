f740e36a85aec1884c3dbfd7d014e48f8b82d1ee
Jamshedjon Sidiqov <j.sidiqov@bueroparallel.de>
2025-01-02 16:40:35 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -1,16 +1,20 @@
 <?php
-
-
 require __DIR__ . '/../vendor/autoload.php';
 
-use \Slim\Factory\AppFactory;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Twig\Loader\FilesystemLoader;
 
 $app = AppFactory::create();
 
+$loader = new FilesystemLoader(__DIR__ . '/../resources/views');
+$twig = new Twig($loader);
 
-$app->get('/', function ($request, $response, $args) {
-    $response->getBody()->write("Hello, world!");
-    return $response;
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/index', function ($request, $response, $args) use ($twig) {
+    return $twig->render($response, 'index.html.twig', ['name' => 'World']);
 });
 
 $app->run();
